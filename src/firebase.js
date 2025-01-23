b488a479acf243d5db51b322a7e535d7ee64046c
Raphael Moura Santiago <raphael.moura@alphacorp.local>
2025-01-23 17:27:27 -0400

    Ajuste de segurança

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signOut } from "firebase/auth";
 import { getFirestore, doc, getDoc, collection, getDocs, deleteDoc } from "firebase/firestore";
 
+// Obtendo as variáveis de ambiente do arquivo .env
 const firebaseConfig = {
-    apiKey: "AIzaSyApQNqkaP6BazjF7lmx0sFhItoy6DfDjec",
-    authDomain: "torneio-97841.firebaseapp.com",
-    projectId: "torneio-97841",
-    storageBucket: "torneio-97841.firebasestorage.app",
-    messagingSenderId: "618591947829",
-    appId: "1:618591947829:web:a7119768207be7bbe3357c"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
